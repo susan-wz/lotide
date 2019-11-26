664ef415afbdae908a968d4d1adecbe7c99317c6
Susan Zhang <susanwenzhang@gmail.com>
2019-11-26 20:31:43 +0000

    adding assertArrayEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,28 @@
+/* INSTRUCTIONS
+Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+*/
+
+// eqArrays function
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  } return true;
+}
+
+const assertArraysEqual = function(arr1, arr2, expected) {
+  if (eqArrays(arr1,arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+// TEST CODE
+
+assertArraysEqual([1, 2, 3], [1, 4, 5], true);
